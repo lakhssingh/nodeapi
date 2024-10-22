252a59d30ada2ae5014ab11e52db98d7a75f3c43
Lakhs <lakhbir.shah@yahoo.co.uk>
2024-10-22 10:15:47 +0100

    created auth, signin, create user

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import app from "./server";
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("app started");
 });
--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/product", (req, res) => {
-  res.json({ message: "Hello" });
+  res.json({ message: "hello" });
 });
 router.get("/product/:id", () => {});
 router.put("/product/:id", () => {});
